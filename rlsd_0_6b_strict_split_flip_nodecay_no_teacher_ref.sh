e1f9f6ee2f3acd74b927f46a90824fc3b0ec5deb
liyang <guzhoudiaoxue0706@163.com>
2026-05-21 22:57:55 +0800

    need 8b

--- rlsd_0_6b_strict_split_flip_nodecay_no_teacher_ref.sh
+++ rlsd_0_6b_strict_split_flip_nodecay_no_teacher_ref.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:2
 #SBATCH --mem-per-cpu=81920M
 #SBATCH --time=72:00:00
-#SBATCH --exclude=gpua800n24,gpua800n04
+#SBATCH --exclude=gpua800n24,gpua800n04,gpua800n07,gpua800n06,gpua800n23,gpua800n03,gpua800n25,gpua800n21
 set -eo pipefail
 nvidia-smi
 
